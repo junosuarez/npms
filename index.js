d939f8cd0bbc0071b4ccb1dc7a72e47945d22ea4
jden <jason@denizac.org>
2013-05-12 00:03:32 -0700

    dont require explicit version number when installing from github

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 #! /usr/local/bin/node
 var spawn = require('child_process').spawn
+var ghLatest = require('github-latest')
+var Q = require('q')
 
 if (process.argv.some(function (arg) {
   return arg.toLowerCase() === '-h' || arg.toLowerCase() === '--help'
@@ -12,15 +14,18 @@ if (process.argv.some(function (arg) {
     '\n\n\tIt saves to packages.json by default, it uses github for git urls by default, and it\n\tuses git+ssh by default, because why wouldn\'t you?')
 }
 
-var packages = process.argv.slice(2).map(format)
+var packages = process.argv.slice(2)
 
 if (!packages.length) {
   die('no packages specified; aborting installation')
 }
 
-console.log('installing: ' + packages.join(', '))
-install(packages)
+Q.all(packages.map(format)).then(function (packages) {
 
+  console.log('installing: ' + packages.join(', '))
+  install(packages)
+
+})
 
 /////////////////////
 
@@ -33,16 +38,26 @@ function install(packages) {
 
 function format(package) {
   if (/.+\/.+/.test(package)) {
-    console.log('git:', package)
-    package = package.split(/@|#/)
-    if (!package[1]){
-      die('No tag specified for ' + package + ' ( user/repo@tag )')
+    package = parse(package)
+    if (!package.tag){
+      return Q.nfcall(ghLatest, package.user, package.repo)
+        .then(function (tag) {
+          return 'git+ssh://git@github.com:' + package.user + '/' + package.repo + '.git#' + tag
+        })
     }
-    return 'git+ssh://git@github.com:'+package[0] + '.git#' + package[1]
+    return Q('git+ssh://git@github.com:' + package.user + '/' + package.repo + '.git#' + package.tag)
   }
-  return package
+  return Q(package)
 }
 
+function parse(package) {
+  var parts = package.split(/@|#|\//g)
+  return {
+    user: parts[0],
+    repo: parts[1],
+    tag: parts[2]
+  }
+}
 
 function die(m, code) {
   console.log(m)
